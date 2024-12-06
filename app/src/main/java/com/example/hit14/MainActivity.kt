edc1826fcb19104c7b9c961cf515d99ae5ae8f17
Jasung Lee <wotjd7661@gmail.com>
2024-12-07 02:43:14 +0900

    f1

---
+++ app/src/main/java/com/example/hit14/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.hit14
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    // 안녕하세요 깃헙 올려볼게요
+}
